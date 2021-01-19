e6a2d2fa038e6cf3facbc3bdf0e34c817306e18d
DELL <DELL@DESKTOP-G7H3AIM>
2021-01-19 20:21:20 +0530

    first commit

---
+++ ImageUploadingVideo26/src/main/java/com/ashok/demo/ImageUploadingVideo26Application.java
@@ -0,0 +1,13 @@
+package com.ashok.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageUploadingVideo26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageUploadingVideo26Application.class, args);
+	}
+
+}
---
+++ ImageUploadingVideo26/src/test/java/com/ashok/demo/ImageUploadingVideo26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashok.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageUploadingVideo26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
